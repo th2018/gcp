c175512e567221244dcd6ba6ccec7806c6ccf20d
Tao Huang <huangt@caryinstitute.org>
2022-05-28 11:49:31 -0600

    Add files via upload

---
+++ camera.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 27 15:51:06 2022
+
+@author: taohuang
+"""
+
+import pandas as pd
+
+camera=pd.read_csv(r"H:\20200610_wbs1_macro\camera_locations.txt")
