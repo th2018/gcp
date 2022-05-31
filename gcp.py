b4aa5624f47a154b1ca4a41a62ece7f3b0b96b18
Tao Huang <th2018@wildcats.unh.edu>
2022-05-31 14:18:52 -0600

    Update gcp.py

--- gcp.py
+++ gcp.py
@@ -1,17 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
 import os
-
 import matplotlib.pyplot as plt
 import pandas as pd
-
+#read csv
 gcp=pd.read_csv(r"H:\20200610_wbs1_macro\gcp\20200609nancypaired_15gcp.csv")
-
+#assign index
 gcp2=gcp.assign(index=gcp.index)
-
+#export to csv
 gcp2.to_csv(r"H:\20200610_wbs1_macro\gcp\20200609nancypaired_15gcp_index.csv")
