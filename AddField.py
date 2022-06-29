a98e3feb920a3960ca2be8a4a4a717a2e9d6e3f3
Tao Huang <huangt@caryinstitute.org>
2022-06-29 17:14:44 -0600

    Add files via upload

---
+++ AddField.py
@@ -0,0 +1,7 @@
+arcpy.AddField_management("uavgcp","lat","DOUBLE")
+arcpy.AddField_management("uavgcp","lon","DOUBLE")
+arcpy.AddField_management("uavgcp","ele","DOUBLE")
+arcpy.AddField_management("uavgcp","x","DOUBLE")
+arcpy.AddField_management("uavgcp","y","DOUBLE")
+arcpy.AddField_management("uavgcp","z","DOUBLE")
+
